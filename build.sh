63f3d5d3ea2890ccb568d1e37677d6ee47aae528
BearFromMars <melchi@garfias.org>
2023-05-06 02:01:22 -0700

    pushing updates to github & bear.oops.wtf

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -arP --delete public/ bear@bear.oops.wtf:/mnt/www/front/public
+rsync -arP --delete public bear@bear.oops.wtf:/mnt/www/front
 
 git add .
 git commit -m "pushing updates to github & bear.oops.wtf"
